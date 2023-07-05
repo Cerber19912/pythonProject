9e80db0eacc32ccb731bceedafde29fede5e13ba
Cerber19912 <cerber19912@gmail.com>
2023-07-05 20:24:55 +0700

    Initial commit

---
+++ Tests/test_pet_friends.py
@@ -0,0 +1,79 @@
+from api import PetFriends
+from settings import valid_email, valid_password
+import os
+
+
+pf = PetFriends()
+
+
+def test_get_api_key_for_valid_user(email=valid_email, password=valid_password):
+    """ Проверяем что запрос api ключа возвращает статус 200 и в тезультате содержится слово key"""
+
+    status, result = pf.get_api_key(email, password)
+
+    assert status == 200
+    assert 'key' in result
+
+
+def test_get_all_pets_with_valid_key(filter=''):
+    """ Проверяем что запрос всех питомцев возвращает не пустой список.
+    Для этого сначала получаем api ключ и сохраняем в переменную auth_key. Далее используя этого ключ
+    запрашиваем список всех питомцев и проверяем что список не пустой.
+    Доступное значение параметра filter - 'my_pets' либо '' """
+
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    status, result = pf.get_list_of_pets(auth_key, filter)
+
+    assert status == 200
+    assert len(result['pets']) > 0
+
+def test_post_create_pet_simple_with_valid_data(name= 'Нора', animal_type= 'Британка',
+                                     age= '10'):
+    """Проверяем что можно добавить питомца с корректными данными"""
+
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+
+    status, result = pf.post_create_pet_simple(auth_key, name, animal_type, age)
+
+    # Сверяем полученный ответ с ожидаемым результатом
+    assert status == 200
+    assert result['name'] == name
+    assert result['animal_type'] == animal_type
+    assert result['age'] == age
+
+def test_successful_delete_self_pet():
+    """Проверяем возможность удаления питомца"""
+
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
+
+
+    if len(my_pets['pets']) == 0:
+        pf.add_new_pet(auth_key, "Шарик", "5")
+        _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
+
+    pet_id = my_pets['pets'][0]['id']
+    status, _ = pf.delete_pet(auth_key, pet_id)
+
+    _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
+
+    assert status == 200
+    assert pet_id not in my_pets.values()
+
+def test_succesful_put_pet_set_photo(pet_photo = 'C:\Users\Ярик\PycharmProjects\pythonProject\Tests\images\pexels-photo-320014.jpeg'):
+    pet_photo = os.path.join(os.path.dirname(__file__), pet_photo)
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
+    status, result = pf.post_pet_set_photo(auth_key, pet_photo)
+    assert status == 200
+
+def test_successful_update_self_pet_info(name='Локи', animal_type = 'Котэ', age = 7):
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
+
+    if len(my_pets['pets']) > 0:
+        status, result = pf.update_pet_info(auth_key, my_pets['pets'][0]['id'], 'Пушистик', 'кот', '7')
+        assert status == 200
+        assert result['name'] == name
+    else:
+        raise Exception("There is no my pets")
